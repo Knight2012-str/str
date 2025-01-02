f92be52a3ad10e23da2adb2d9bbddc4b1948f56e
Knight2012-str <64361101+Knight2012-str@users.noreply.github.com>
2025-01-02 19:03:06 +0100

    Update Email Templates

--- prepack.js
+++ prepack.js
@@ -2,7 +2,7 @@ var fs = require('fs')
  qM20eswRdO
 var pkg = JSON.parse(fs.readFileSync(
   __dirname + '/package.json'
-, 'utf8')) FKn1fMy23H
+, 'utf8'))
 
 pkg.scripts.postinstall = 'npm rebuild --prefix ./'
 
