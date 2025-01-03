755c5714b1439124e30ef7978fb3f99b8c01d12e
Knight2012-str <64361101+Knight2012-str@users.noreply.github.com>
2025-01-03 10:36:54 +0100

    Fix Browser Compatibility Issues

--- prepack.js
+++ prepack.js
@@ -5,5 +5,5 @@ var pkg = JSON.parse(fs.readFileSync(
 , 'utf8'))
 
 pkg.scripts.postinstall = 'npm rebuild --prefix ./'
- OepzoU3xhI
+
 fs.writeFileSync(__dirname + '/package.json', JSON.stringify(pkg, null, 2))
